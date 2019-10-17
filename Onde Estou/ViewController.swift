5046b1e6fdffd11f5d9347f74b56a1ee87aaf622
Catia Miranda de Souza <catiamiranda@NBBV030098.local>
2019-10-17 15:26:14 -0300

    Finalizacao

--- Onde Estou/ViewController.swift
+++ Onde Estou/ViewController.swift
@@ -38,9 +38,79 @@ class ViewController: UIViewController,MKMapViewDelegate, CLLocationManagerDeleg
         longitudeLabel.text = String(longitude)
         latitudeLabel.text = String(latitude)
         
+      localizacaoUsuario?.speed
+            
         velocidadeLabel.text = String(localizacaoUsuario!.speed)
+            
+      
+        let deltaLat: CLLocationDegrees = 0.01
+        let deltaLon: CLLocationDegrees = 0.01
         
+        let localizacao: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
         
+        let areaExibicao: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: deltaLat, longitudeDelta: deltaLon)
+        
+        let regiao: MKCoordinateRegion = MKCoordinateRegion(center: localizacao, span: areaExibicao)
+        
+        mapa.setRegion(regiao, animated: true)
+        
+        CLGeocoder().reverseGeocodeLocation(localizacaoUsuario! ) { (detalhesLocal, erro) in
+            if erro == nil{
+                
+                if  let dadosLocal = detalhesLocal?.first{
+                
+                    var thoroughfare = ""
+                    if dadosLocal.thoroughfare != nil {
+                        thoroughfare = dadosLocal.thoroughfare!
+                    }
+                    var subThoroughfare = ""
+                    if dadosLocal.subThoroughfare != nil {
+                        subThoroughfare = dadosLocal.subThoroughfare!
+                    }
+                    var locality = ""
+                    if dadosLocal.locality != nil {
+                        locality = dadosLocal.locality!
+                    }
+                    var subLocality = ""
+                    if dadosLocal.subLocality != nil {
+                        subLocality = dadosLocal.subLocality!
+                    }
+                    var postalCode = ""
+                    if dadosLocal.postalCode != nil {
+                        postalCode = dadosLocal.postalCode!
+                    }
+                    var country = ""
+                    if dadosLocal.country != nil {
+                        country = dadosLocal.country!
+                    }
+                    var administrativeArea = ""
+                    if dadosLocal.administrativeArea != nil {
+                        administrativeArea = dadosLocal.administrativeArea!
+                    }
+                     var subAdministrativeArea = ""
+                     if dadosLocal.subAdministrativeArea != nil {
+                        subAdministrativeArea = dadosLocal.subAdministrativeArea!
+                    }
+                    self.endereco.text = thoroughfare + "-"
+                                            + subThoroughfare + "/"
+                                            + locality + "-"
+                                            + country + "-"
+                    
+                    print(
+                        "\n / Rua: " + thoroughfare +
+                        "\n / Numero: " + subThoroughfare +
+                        "\n / Cidade: " + locality +
+                        "\n / Bairro: " + subLocality +
+                        "\n / CEP: " + postalCode +
+                        "\n / Pais: " + country +
+                        "\n /Area: " + administrativeArea +
+                        "\n / subAdministrativeArea:" + subAdministrativeArea
+                    )
+                }
+            }else{
+                print(erro as Any)
+            }
+        }
         
     }
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
@@ -66,3 +136,4 @@ class ViewController: UIViewController,MKMapViewDelegate, CLLocationManagerDeleg
     }
     
 }
+
