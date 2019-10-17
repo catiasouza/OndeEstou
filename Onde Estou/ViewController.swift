5f8cbdc9dd7f72cc6d4be4aeba0cca6aa8a79dfe
Catia Miranda de Souza <catiamiranda@NBBV030098.local>
2019-10-17 11:34:22 -0300

    Criando o projeto

---
+++ Onde Estou/ViewController.swift
@@ -0,0 +1,19 @@
+
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController,MKMapViewDelegate {
+    
+    
+    @IBOutlet weak var mapa: MKMapView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+
+}
+
