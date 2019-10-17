a492fc16383d2d529d015cc8c45823fbb1af9669
Catia Miranda de Souza <catiamiranda@NBBV030098.local>
2019-10-17 13:42:00 -0300

    Criando as autorizacoes

--- Onde Estou/ViewController.swift
+++ Onde Estou/ViewController.swift
@@ -3,17 +3,42 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController,MKMapViewDelegate {
+class ViewController: UIViewController,MKMapViewDelegate, CLLocationManagerDelegate{
     
     
     @IBOutlet weak var mapa: MKMapView!
+    var gerenciadorLocalizacao = CLLocationManager()
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        
+        gerenciadorLocalizacao.delegate = self
+        gerenciadorLocalizacao.desiredAccuracy = kCLLocationAccuracyBest //melhor localizacao
+        gerenciadorLocalizacao.requestWhenInUseAuthorization()           //solicitacao
+        gerenciadorLocalizacao.startUpdatingLocation()         //MONITORAR LOCALIZACAO
+        
     }
-
-
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status != .authorizedWhenInUse {
+            
+            let alertaController = UIAlertController(title: "Permissao de localizacao", message: "Necessaria permissao para acesso a sua localizacao! Por favor habilite!", preferredStyle: .alert)
+            
+            let acaoConfiguracao = UIAlertAction(title: "Abrir configuracoes", style: .default, handler: {(alertaConfiguracoes) in
+                if let configuracoes = NSURL(string: UIApplication.openSettingsURLString){
+                    UIApplication.shared.open(configuracoes as URL)
+                }
+            })
+            
+            let acaoCancelar = UIAlertAction(title: "Cancelar", style: .default, handler: nil)
+            
+            alertaController.addAction(acaoConfiguracao)
+            alertaController.addAction(acaoCancelar)
+            
+            present( alertaController, animated: true, completion: nil)
+            
+        }
+        
+    }
+    
 }
-
